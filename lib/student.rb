84aa632926f42c7d910449a64cb4dc6395e09cd4
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-13 17:59:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,8 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL 
+    SELECt * FROM studnets
   end
 
   def self.find_by_name(name)
